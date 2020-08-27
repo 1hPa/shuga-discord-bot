55aa3b1d70693df0d678e2185692edc999ac2936
Yu <tuba0927hy@gmail.com>
2020-08-27 19:59:54 +0900

    feat: add importing os

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,12 @@
 import discord
 from discord.ext import commands
+import os
 
 #class definition
 class Bot(commands.Bot):
+    #call when ready
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print(self.user.id)
+        print('-----')
