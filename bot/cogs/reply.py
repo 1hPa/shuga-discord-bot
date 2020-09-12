593526c31216b7d11446e996a35817a0bb61f588
Yu <tuba0927hy@gmail.com>
2020-09-12 21:54:07 +0900

    feat: add message command

--- bot/cogs/reply.py
+++ bot/cogs/reply.py
@@ -4,3 +4,12 @@ from discord.ext import commands
 class RepCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    #Commands creation
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content == 'こんにちは':
+            await message.channel.send('こんにちは')
