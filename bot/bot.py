f270bd75b4a59de20e59f8ec84b35b580d65f930
Yu <tuba0927hy@gmail.com>
2020-09-12 22:02:29 +0900

    feat: add Implemented passing to superclass

--- bot/bot.py
+++ bot/bot.py
@@ -10,6 +10,13 @@ INITIAL_EXTENSIONS = [
 
 #class definition
 class Bot(commands.Bot):
+
+    #Bot Constructor
+    def __init__(self, command_prefix):
+
+        #Pass to the superclass and run
+        super().__init__(command_prefix)
+
     #call when ready
     async def on_ready(self):
         print('-----')
