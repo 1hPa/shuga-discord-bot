fcbc09134351b42b7e6acf907383fec07a4114b1
Yu <tuba0927hy@gmail.com>
2020-09-15 00:50:01 +0900

    feat: add delete function

--- bot/cogs/delete.py
+++ bot/cogs/delete.py
@@ -1,8 +1,18 @@
 from discord.ext import commands
 import discord
 
+SENSITIVE_WORD = [
+    'ちんちん'
+]
+
 #Definition class
 class DeleteCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def on_message(self, ctx, message):
+        for improper in SENSITIVE_WORD:
+            if message==improper:
+                await ctx.message.delete()
