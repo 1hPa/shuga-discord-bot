77c6bfe6e161b2afd628ced844eb4ddaa4bff1fa
Yu <tuba0927hy@gmail.com>
2020-09-16 00:31:16 +0900

    feat: add making command

--- bot/cogs/quote.py
+++ bot/cogs/quote.py
@@ -3,3 +3,10 @@ from discord.ext import commands
 from datetime import datetime 
 
 class QuoteCog(commands.Cog):
+    #constructor
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def quote(self, ctx):
+        await ctx.send(random.choice(KOSEN_QUOTE))
