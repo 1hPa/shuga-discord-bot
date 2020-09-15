a0144b74e45bf8bf42f12e2f64722feb3649e630
Yu <tuba0927hy@gmail.com>
2020-09-15 21:35:42 +0900

    feat: change decorator

--- bot/cogs/delete.py
+++ bot/cogs/delete.py
@@ -11,7 +11,7 @@ class DeleteCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
