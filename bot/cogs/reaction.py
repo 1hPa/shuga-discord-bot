eba746c3f73b6cb6384c7723a608f91a4ef1c303
Yu <tuba0927hy@gmail.com>
2020-09-16 03:56:55 +0900

    feat: add image transmission process

--- bot/cogs/reaction.py
+++ bot/cogs/reaction.py
@@ -4,3 +4,11 @@ import discord
 class RxnCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if GRASS in message.content:
+            await message.channel.send(../../img/grass.png)
