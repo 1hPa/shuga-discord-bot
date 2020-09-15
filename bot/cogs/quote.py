3147b7c08e418bd61e1ad49e674ce6dce02c7562
Yu <tuba0927hy@gmail.com>
2020-09-16 00:35:43 +0900

    feat: add loading function

--- bot/cogs/quote.py
+++ bot/cogs/quote.py
@@ -15,3 +15,6 @@ class QuoteCog(commands.Cog):
     @commands.command()
     async def quote(self, ctx):
         await ctx.send(random.choice(KOSEN_QUOTE))
+
+def setup(bot):
+    bot.add_cog(QuoteCog(bot))
