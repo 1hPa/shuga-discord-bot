ed26e3916e92c7f67ae50e59092a7ee5221c0b13
Yu <tuba0927hy@gmail.com>
2020-09-16 01:52:28 +0900

    feat: add loop processing

--- bot/cogs/greeting.py
+++ bot/cogs/greeting.py
@@ -1,4 +1,18 @@
 from discord.ext import tasks, commands
 
+CHANNEL_ID = 728821929103851521
+
 #Definition class
 class GreetingCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @tasks.loop(seconds=60)
+    async def loop():
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')
+#Loop processing
+loop.start()
+
+def setup(bot):
+    bot.add_cog(GreetingCog(bot))
