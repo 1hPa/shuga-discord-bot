c10f64737fc39b7052fac84a309770e35b621acc
Yu <tuba0927hy@gmail.com>
2020-09-15 22:20:58 +0900

    feat: change mention method

--- bot/cogs/delete.py
+++ bot/cogs/delete.py
@@ -18,7 +18,7 @@ class DeleteCog(commands.Cog):
 
         if message.content in SENSITIVE_WORD:
             await message.delete()
-            await message.channel.send('@'message.author.name+'の不適切なメッセージを削除しました。')
+            await message.channel.send(message.author.mention+'の不適切なメッセージを削除しました。')
 
 def setup(bot):
     bot.add_cog(DeleteCog(bot))
