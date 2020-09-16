80775b606328b30abb2a17a4109c3885f2e73fb8
Yu <tuba0927hy@gmail.com>
2020-09-16 20:32:21 +0900

    feat: add ability to display typing

--- bot/cogs/reply.py
+++ bot/cogs/reply.py
@@ -10,15 +10,16 @@ class RepCog(commands.Cog):
 
     #Commands creation
     @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot:
-            return
+    async def on_message(self, ctx, message):
+        async with ctx.typing():
+            if message.author.bot:
+                return
 
-        if message.content == 'こんにちは':
-            await message.channel.send('こんにちは')
+            if message.content == 'こんにちは':
+                await message.channel.send('こんにちは')
 
-        if 'w' in message.content:
-            await asyncio.sleep(5)
-            await message.channel.send(random.choice(('www', '草')))
+            if 'w' in message.content:
+                await asyncio.sleep(5)
+                await message.channel.send(random.choice(('www', '草')))
 def setup(bot):
     bot.add_cog(RepCog(bot))
