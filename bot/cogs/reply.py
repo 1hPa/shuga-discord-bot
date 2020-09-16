9ac68481a9b2fb340b08c26c71d088244bd4f6d9
Yu <tuba0927hy@gmail.com>
2020-09-16 21:03:59 +0900

    feat: add adapt to all

--- bot/cogs/reply.py
+++ bot/cogs/reply.py
@@ -11,15 +11,16 @@ class RepCog(commands.Cog):
     #Commands creation
     @commands.Cog.listener()
     async def on_message(self, ctx, message):
-        async with ctx.typing():
-            await asyncio.sleep(5)
-            if message.author.bot:
-                return
 
-            if message.content == 'こんにちは':
-                await ctx.send('こんにちは')
+        await asyncio.sleep(5)
 
-            if 'w' in message.content:
-                await ctx.send(random.choice(('www', '草')))
+        if message.author.bot:
+            return
+
+        if message.content == 'こんにちは':
+            await ctx.send('こんにちは')
+
+        if 'w' in message.content:
+            await ctx.send(random.choice(('www', '草')))
 def setup(bot):
     bot.add_cog(RepCog(bot))
