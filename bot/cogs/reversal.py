27d700dbafd2ac0ff83e281ee48d73072082c092
Yu <tuba0927hy@gmail.com>
2020-09-28 20:25:00 +0900

    feat: add message that it was used

--- bot/cogs/reversal.py
+++ bot/cogs/reversal.py
@@ -12,5 +12,6 @@ class ReversalCog(commands.Cog):
         strlist = msg
         await ctx.send(''.join(list(reversed(strlist)))+'\nBy'+ctx.author.mention)
         await ctx.message.delete()
+        await ctx.send('reverseコマンドを使用しました。')
 def setup(bot):
     bot.add_cog(ReversalCog(bot))
