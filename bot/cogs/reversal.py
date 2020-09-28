09635342d1808eecf0115d2d69610ea4681e41ed
Yu <tuba0927hy@gmail.com>
2020-09-28 16:35:39 +0900

    feat(command): add reversal command

--- bot/cogs/reversal.py
+++ bot/cogs/reversal.py
@@ -2,7 +2,13 @@ import discord
 from discord.ext import commands
 
 #Definition class
-class RevertCog(commands.Cog):
+class ReversalCog(commands.Cog):
     #constructor
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def reversal(self, ctx, msg):
+        strlist = msg
+        await ctx.send(''.join(list(reversed(strlist)))+'\nBy'+ctx.author.mention)
+        await ctx.message.delete()
